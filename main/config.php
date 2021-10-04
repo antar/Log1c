7604f991bc8c0bce0edcddefdf0072d23ff46ae2
Alessio Carcavalo <toobrokeforrysll@gmail.com>
2021-10-04 09:02:14 +0200

    feat: added config file

---
+++ main/config.php
@@ -0,0 +1,18 @@
+<?php
+// database hostname
+define('db_host','');
+// database username
+define('db_user','');
+// database password
+define('db_pass','');
+// database name
+define('db_name','log1c');
+// database charset
+define('db_charset','utf8');
+// account activation required?
+define('account_activation',true);
+// mail from
+define('mail_from','');
+// activation link
+define('activation_link','https://yourdomain/activate.php');
+?>
