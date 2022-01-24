cbe55040dea3836e253f03db8e3940b042ad2ae5
antar <69793174+antar@users.noreply.github.com>
2022-01-24 10:50:29 +0100

    feat: added recaptcha on server-side

--- src/register.php
+++ src/register.php
@@ -1,5 +1,6 @@
 <?php
 include 'main.php';
+
 // checker for correct inputs
 if (!isset($_POST['username'], $_POST['password'], $_POST['cpassword'], $_POST['email'])) {
 	exit('Please complete the registration form!');
@@ -11,7 +12,7 @@ if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 	exit('Email is not valid!');
 }
 if (!preg_match('/^[a-zA-Z0-9]+$/', $_POST['username'])) {
-    exit('Username is not valid!');
+	exit('Username is not valid!');
 }
 if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
 	exit('Password must be between 5 and 20 characters long!');
@@ -19,22 +20,38 @@ if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
 if ($_POST['cpassword'] != $_POST['password']) {
 	exit('Passwords do not match!');
 }
-// insert data into database
-$stmt = $pdo->prepare('SELECT id, password FROM accounts WHERE username = ? OR email = ?');
-$stmt->execute([ $_POST['username'], $_POST['email'] ]);
-$account = $stmt->fetch(PDO::FETCH_ASSOC);
-if ($account) {
-	echo 'Username and/or email exists!';
-} else {
-	$stmt = $pdo->prepare('INSERT INTO accounts (username, password, email, activation_code) VALUES (?, ?, ?, ?)');
-	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-	$uniqid = account_activation ? uniqid() : 'activated';
-	$stmt->execute([ $_POST['username'], $password, $_POST['email'], $uniqid ]);
-	if (account_activation) {
-		send_activation_email($_POST['email'], $uniqid);
-		echo 'Please check your email to activate your account!';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['recaptcha_response'])) {
+	
+	$recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+	$recaptcha_secret = '6LekZTMeAAAAAIq385wVXvfiVnTO3Qj4gPUA3LR6';
+	$recaptcha_response = $_POST['recaptcha_response'];
+	
+	$recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+	$recaptcha = json_decode($recaptcha);
+	
+	// Take action based on the score returned:
+	if ($recaptcha->score >= 0.5) {
+		// insert data into database
+		$stmt = $pdo->prepare('SELECT id, password FROM accounts WHERE username = ? OR email = ?');
+		$stmt->execute([ $_POST['username'], $_POST['email'] ]);
+		$account = $stmt->fetch(PDO::FETCH_ASSOC);
+		if ($account) {
+			echo 'Username and/or email exists!';
+		} else {
+			$stmt = $pdo->prepare('INSERT INTO accounts (username, password, email, activation_code) VALUES (?, ?, ?, ?)');
+			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+			$uniqid = account_activation ? uniqid() : 'activated';
+			$stmt->execute([ $_POST['username'], $password, $_POST['email'], $uniqid ]);
+			if (account_activation) {
+				send_activation_email($_POST['email'], $uniqid);
+				echo 'Please check your email to activate your account!';
+			} else {
+				echo 'You have successfully registered, you can now login!';
+			}
+		}
 	} else {
-		echo 'You have successfully registered, you can now login!';
+		echo 'NT, sucker!'
 	}
 }
 ?>
